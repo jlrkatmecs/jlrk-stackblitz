10948d272158436393b940ca9a17342db7029451
jlrkatmecs <123176039+jlrkatmecs@users.noreply.github.com>
2023-07-26 20:11:50 +0530

    3 files modified, 1 added, 1 deleted

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 // run `node index.js` in the terminal
 const http = require('http');
 const path = require('path');
@@ -34,4 +35,21 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {
   logMsg('Server running on PORT ${PORT}');
+});
+*/
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('^/$|index(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
 });
